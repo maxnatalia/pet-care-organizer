16d2281d63082d6c88f831dd5f3ffb3853df5e3b
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-18 09:01:59 +0200

    created Footer component

---
+++ src/features/Layout/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { dynamicYear, email, socialMedia } from "./footerData";
+import { StyledFooter, ContactBox, Mail, Author } from "./styled";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <ContactBox>
+        <Mail href={`mailto:${email}`} title={`Mail to: ${email}`}>
+          {email}
+        </Mail>
+        <div>
+          {socialMedia.map(({ name, link, Icon }) => (
+            <a
+              key={name}
+              href={link}
+              title={name}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon />
+            </a>
+          ))}
+        </div>
+      </ContactBox>
+      <Author>
+        Project and design by Natalia Mazur-Żurek
+        <div> &copy; {dynamicYear} All Rights Reserved</div>
+      </Author>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
