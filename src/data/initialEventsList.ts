294b89f34f942ca3a09ec61c7d6041576f242306
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-21 10:22:39 +0200

    added initialEventsList for initial data

---
+++ src/data/initialEventsList.ts
@@ -0,0 +1,31 @@
+import { Event } from "../types/types";
+
+export const initialEventsList: Event[] = [
+  {
+    eventId: "event1Frida",
+    eventPetId: "1Frida",
+    eventCategory: "groomer",
+    eventDescription: "wizyta u groomera",
+    eventDate: "2024-05-10",
+    eventAddDate: "2024-04-18",
+    eventUpdateDate: "",
+  },
+  {
+    eventId: "event2Frida",
+    eventPetId: "1Frida",
+    eventCategory: "show",
+    eventDescription: "show",
+    eventDate: "2024-07-20",
+    eventAddDate: "2024-04-18",
+    eventUpdateDate: "",
+  },
+  {
+    eventId: "event1Rio",
+    eventPetId: "2Rio",
+    eventCategory: "show",
+    eventDescription: "show",
+    eventDate: "2024-05-23",
+    eventAddDate: "2024-04-18",
+    eventUpdateDate: "",
+  },
+];
