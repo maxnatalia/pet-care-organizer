7901d9fc2902f3ca21091ceb31300230cacf9311
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-21 10:21:03 +0200

    added navigationData for Navigation component

---
+++ src/features/Layout/Navigation/navigationList.ts
@@ -0,0 +1,36 @@
+import { TbHome, TbPaw } from "react-icons/tb";
+import { MdAddCircle, MdEvent } from "react-icons/md";
+import { styledIcon } from "./styled";
+
+export const navigationList = [
+  {
+    name: "Home",
+    link: "/",
+    title: "Go Home",
+    Icon: styledIcon(TbHome),
+  },
+  {
+    name: "My Pets",
+    link: "/pets",
+    title: "Go to pets list",
+    Icon: styledIcon(TbPaw),
+  },
+  {
+    name: "Events",
+    link: "/events",
+    title: "Go to events list",
+    Icon: styledIcon(MdEvent),
+  },
+  {
+    name: "Add Pet",
+    link: "/petForm",
+    title: "Go to form",
+    Icon: styledIcon(MdAddCircle),
+  },
+  {
+    name: "Add Event",
+    link: "/eventForm",
+    title: "Go to form",
+    Icon: styledIcon(MdAddCircle),
+  },
+];
