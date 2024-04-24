3ea67602a4b65c61ff51adf26ff271d4027928bc
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-24 10:58:25 +0200

    updated - added new props $center

--- src/components/Section/styled.ts
+++ src/components/Section/styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledSection = styled.section`
   padding: 20px;
@@ -15,5 +15,30 @@ export const SectionHeader = styled.h2`
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 16px;
+  }
+`;
+
+export const Wrapper = styled.div<{ $center?: boolean }>`
+  display: flex;
+  flex-direction: column;
+
+  ${({ $center }) =>
+    $center &&
+    css`
+      justify-content: center;
+      align-items: center;
+    `}
+`;
+
+export const Title = styled.h3`
+  margin-top: 0;
+  margin-bottom: 20px;
+  text-transform: capitalize;
+  font-size: 20px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.textPrimary};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 14px;
   }
 `;
