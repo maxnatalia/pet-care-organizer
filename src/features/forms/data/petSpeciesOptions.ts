1ae413a946989465671d0a356465fff1810b3439
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-24 10:59:34 +0200

    created petSpeciesOptions for select field in form

---
+++ src/features/forms/data/petSpeciesOptions.ts
@@ -0,0 +1,9 @@
+export const petSepeciesOptions = [
+  { label: "❔ Other", value: "other" },
+  { label: "🐶 Dog", value: "dog" },
+  { label: "😺 Cat", value: "cat" },
+  { label: "🐰 Rabbit", value: "rabbit" },
+  { label: "🦜 Parrot", value: "parrot" },
+  { label: "🐭 Rodent", value: "rodent" },
+  { label: "🐠 Fish", value: "fish" },
+];
