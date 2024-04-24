8cccd6b966f1d4a07ad10f2b0b4f7b9fab4417be
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-24 10:59:08 +0200

    created eventTypesOptions for select field in form

---
+++ src/features/forms/data/eventTypesOptions.ts
@@ -0,0 +1,7 @@
+export const eventTypesOptions = [
+  { label: "❔ Unspecified", value: "unspecified" },
+  { label: "🥼 Vet", value: "vet" },
+  { label: "🛁 Groomer", value: "groomer" },
+  { label: "🥇 Show", value: "show" },
+  { label: "🎓 Training", value: "training" },
+];
