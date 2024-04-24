6198f148717417698b8b82d805c04f7a7b466d22
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-24 10:57:27 +0200

    deleted unnecessary files

--- src/data/petSpeciesCategories.ts
+++
@@ -1,9 +0,0 @@
-export const petSepeciesCategories = [
-  { label: "❔ Other", value: "other" },
-  { label: "🐶 Dog", value: "dog" },
-  { label: "😺 Cat", value: "cat" },
-  { label: "🐰 Rabbit", value: "rabbit" },
-  { label: "🦜 Parrot", value: "parrot" },
-  { label: "🐭 Rodent", value: "rodent" },
-  { label: "🐠 Fish", value: "fish" },
-];
