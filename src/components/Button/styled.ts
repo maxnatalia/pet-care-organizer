05afe956bfc36999081df913d485c8c5d6df3ab6
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-26 10:22:02 +0200

    updated - added new props edit and delete

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,19 +1,22 @@
 import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button<{ $special?: boolean }>`
+export const StyledButton = styled.button<{
+  $special?: boolean;
+  $edit?: boolean;
+  $delete?: boolean;
+}>`
   margin: 10px;
   padding: 6px 10px;
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 6px;
   font-size: 18px;
   font-weight: 700;
   text-transform: uppercase;
   color: ${({ theme }) => theme.color.special};
   background-color: ${({ theme }) => theme.color.backgroundSecondary};
   border-radius: 10px;
-  border: 1px solid ${({ theme }) => theme.color.special};
+  border: 2px solid ${({ theme }) => theme.color.special};
   cursor: pointer;
   transition: all 0.5s ease-in-out;
 
@@ -29,9 +32,8 @@ export const StyledButton = styled.button<{ $special?: boolean }>`
   ${({ $special }) =>
     $special &&
     css`
-      /* position: absolute;
-      right: 0;
-      top: 0; */
+      width: 40px;
+      height: 40px;
       border-radius: 50%;
       background-color: ${({ theme }) => theme.color.special};
       color: ${({ theme }) => theme.color.backgroundPrimary};
@@ -39,6 +41,34 @@ export const StyledButton = styled.button<{ $special?: boolean }>`
       &:hover {
         color: ${({ theme }) => theme.color.special};
         background-color: ${({ theme }) => theme.color.backgroundSecondary};
+      }
+    `}
+
+  ${({ $edit }) =>
+    $edit &&
+    css`
+      border-radius: 50%;
+      border: 2px solid ${({ theme }) => theme.color.edit};
+      color: ${({ theme }) => theme.color.backgroundPrimary};
+      background-color: ${({ theme }) => theme.color.edit};
+
+      &:hover {
+        background-color: ${({ theme }) => theme.color.backgroundSecondary};
+        color: ${({ theme }) => theme.color.edit};
+      }
+    `}
+
+    ${({ $delete }) =>
+    $delete &&
+    css`
+      border-radius: 50%;
+      border: 2px solid ${({ theme }) => theme.color.delete};
+      color: ${({ theme }) => theme.color.backgroundPrimary};
+      background-color: ${({ theme }) => theme.color.delete};
+
+      &:hover {
+        background-color: ${({ theme }) => theme.color.backgroundSecondary};
+        color: ${({ theme }) => theme.color.delete};
       }
     `}
 `;
