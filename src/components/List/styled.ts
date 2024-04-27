ea2a1a66059d786975e2b3f624f8095233788250
maxnatalia <Nataliamazur1988@gmail.com>
2024-04-27 06:58:30 +0200

    added styles for List component

---
+++ src/components/List/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledList = styled.ul`
+  list-style: none;
+  padding: 40px 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  gap: 40px;
+  background-color: ${({ theme }) => theme.color.backgroundSecondary};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    padding: 20px 14px;
+    gap: 20px;
+  }
+`;
