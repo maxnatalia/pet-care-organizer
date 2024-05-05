8f955fa34c145663dfcd86ecd2654a2989653a80
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-05 09:11:12 +0200

    created NotFound component

--- src/features/pages/NotFound/index.tsx
+++ src/features/pages/NotFound/index.tsx
@@ -1,16 +1,7 @@
-import Button from "../../../components/Button";
-import Section from "../../../components/Section";
-import { Image } from "../../../components/Image";
-import errorIcon from "./errorIcon.svg";
+import Problem from "../../../components/Problem";
 
 const NotFound = () => {
-  return (
-    <Section header="page not found - 404" title="Ooops...don't worry." $center>
-      <Image src={errorIcon} alt="Cat Error Icon" />
-      <p>The page you're looking for isn't available. Please return to Home.</p>
-      <Button redirectPath="/">Go Home</Button>
-    </Section>
-  );
+  return <Problem error />;
 };
 
 export default NotFound;
