42c4ac57944e57ab24cf319502656f8dc5155988
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-08 11:29:30 +0200

    improved - adjusted to new props

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,9 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const StyledButton = styled.button<{
-  $special?: boolean;
-  $edit?: boolean;
-  $delete?: boolean;
+  $type: "edit" | "delete" | "special" | "default";
 }>`
   margin: 10px;
   padding: 6px 10px;
@@ -13,62 +11,64 @@ export const StyledButton = styled.button<{
   font-size: 18px;
   font-weight: 700;
   text-transform: uppercase;
-  color: ${({ theme }) => theme.color.special};
-  background-color: ${({ theme }) => theme.color.backgroundSecondary};
-  border-radius: 10px;
-  border: 2px solid ${({ theme }) => theme.color.special};
+  border-width: 2px;
+  border-style: solid;
   cursor: pointer;
   transition: all 0.5s ease-in-out;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.special};
-    color: ${({ theme }) => theme.color.backgroundPrimary};
-  }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 14px;
   }
 
-  ${({ $special }) =>
-    $special &&
+  ${({ $type }) =>
+    $type === "default" &&
     css`
-      width: 40px;
-      height: 40px;
-      border-radius: 50%;
-      background-color: ${({ theme }) => theme.color.special};
-      color: ${({ theme }) => theme.color.backgroundPrimary};
+      border-color: ${({ theme }) => theme.color.textPrimary};
+      color: ${({ theme }) => theme.color.textPrimary};
+      background-color: ${({ theme }) => theme.color.backgroundSecondary};
 
       &:hover {
-        color: ${({ theme }) => theme.color.special};
-        background-color: ${({ theme }) => theme.color.backgroundSecondary};
+        background-color: ${({ theme }) => theme.color.textPrimary};
+        color: ${({ theme }) => theme.color.backgroundSecondary};
       }
     `}
 
-  ${({ $edit }) =>
-    $edit &&
+  ${({ $type }) =>
+    $type === "special" &&
     css`
-      border-radius: 50%;
-      border: 2px solid ${({ theme }) => theme.color.edit};
-      color: ${({ theme }) => theme.color.backgroundPrimary};
-      background-color: ${({ theme }) => theme.color.edit};
+      border-color: ${({ theme }) => theme.color.special};
+      color: ${({ theme }) => theme.color.special};
+      background-color: ${({ theme }) => theme.color.backgroundSecondary};
 
       &:hover {
-        background-color: ${({ theme }) => theme.color.backgroundSecondary};
-        color: ${({ theme }) => theme.color.edit};
+        background-color: ${({ theme }) => theme.color.special};
+        color: ${({ theme }) => theme.color.backgroundPrimary};
       }
     `}
 
-    ${({ $delete }) =>
-    $delete &&
+  ${({ $type }) =>
+    $type === "edit" &&
     css`
-      border-radius: 50%;
-      border: 2px solid ${({ theme }) => theme.color.delete};
-      color: ${({ theme }) => theme.color.backgroundPrimary};
-      background-color: ${({ theme }) => theme.color.delete};
+      border-color: ${({ theme }) => theme.color.edit};
+      color: ${({ theme }) => theme.color.edit};
+      background-color: ${({ theme }) => theme.color.backgroundSecondary};
 
       &:hover {
-        background-color: ${({ theme }) => theme.color.backgroundSecondary};
-        color: ${({ theme }) => theme.color.delete};
+        background-color: ${({ theme }) => theme.color.edit};
+        color: ${({ theme }) => theme.color.backgroundPrimary};
+      }
+    `}
+
+    ${({ $type }) =>
+    $type === "delete" &&
+    css`
+      border-color: ${({ theme }) => theme.color.delete};
+      color: ${({ theme }) => theme.color.delete};
+      background-color: ${({ theme }) => theme.color.backgroundSecondary};
+
+      &:hover {
+        background-color: ${({ theme }) => theme.color.delete};
+        color: ${({ theme }) => theme.color.backgroundPrimary};
       }
     `}
 `;
