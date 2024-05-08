e6c1781bd2dc4ea4cddb0bd6fbdb3c8a9f7f5023
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-08 11:30:20 +0200

    improved - deleted CloseIcon styles

--- src/components/Drawer/styled.ts
+++ src/components/Drawer/styled.ts
@@ -1,26 +1,14 @@
 import styled from "styled-components";
-import { FaRegWindowClose } from "react-icons/fa";
 
 export const StyledDrawer = styled.div`
+  padding: 6px 10px;
   background-color: ${({ theme }) => theme.color.backgroundSecondary};
   border-radius: 10px;
+  border: 4px solid ${({ theme }) => theme.color.backgroundPrimary};
   display: flex;
   justify-content: center;
   position: absolute;
-  top: 80px;
+  top: -30px;
   right: 10px;
-`;
-
-export const CloseIcon = styled(FaRegWindowClose)`
-  align-self: center;
-  margin: 10px;
-  font-size: 30px;
-  color: ${({ theme }) => theme.color.special};
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    color: ${({ theme }) => theme.color.backgroundPrimary};
-    background-color: ${({ theme }) => theme.color.special};
-  }
+  z-index: 2;
 `;
