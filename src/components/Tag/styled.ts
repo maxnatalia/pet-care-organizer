3afcbe532c696e9f9d8bedf5908cc4199c1f42cd
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-10 11:18:12 +0200

    added basic styles for Tag component

---
+++ src/components/Tag/styled.ts
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledTag = styled(Link)`
+  margin: 10px;
+  padding: 4px 10px;
+  display: inline-flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  border-radius: 6px;
+  border: 4px solid yellow;
+`;
