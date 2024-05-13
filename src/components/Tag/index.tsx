f771ad3d3c399d46cc9232bb962c102ca93b9e91
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-13 10:22:53 +0200

    updated - added new props title

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -4,11 +4,13 @@ type TagProps = {
   path: string;
   icon: string;
   tagName: string;
+  title: string;
+  $special?: boolean;
 };
 
-const Tag = ({ path, icon, tagName }: TagProps) => {
+const Tag = ({ path, icon, tagName, title, $special }: TagProps) => {
   return (
-    <StyledTag to={path}>
+    <StyledTag to={path} title={title} $special={$special}>
       <span>{icon}</span>
       {tagName}
     </StyledTag>
