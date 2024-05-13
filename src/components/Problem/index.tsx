849420cb16e45c5770b1e28a6726ab18405653df
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-13 10:23:52 +0200

    updated Problem component - improved props

--- src/components/Problem/index.tsx
+++ src/components/Problem/index.tsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { MdHome } from "react-icons/md";
 import Button from "../Button";
 import Section from "../Section";
-import { Image } from "./styled";
 import petIcon from "./petIcon.svg";
+import { Image } from "./styled";
 
 type ProblemProps = {
   error?: boolean;
 };
 
 const Problem = ({ error = false }: ProblemProps) => {
+  const navigate = useNavigate();
   return (
     <Section
       header={error ? "page not found - 404" : "Empty List"}
@@ -15,13 +18,16 @@ const Problem = ({ error = false }: ProblemProps) => {
       $center
     >
       <Image src={petIcon} alt="Cat Error Icon" />
-      <p>Start adding items to the list.</p>
+      {!error && <p>Start adding items to the list.</p>}
       {error && (
         <p>
           The page you're looking for isn't available. Please return to Home.
         </p>
       )}
-      <Button redirectPath="/">Go Home</Button>
+      <Button $variety="default" title="Go Home" onClick={() => navigate("/")}>
+        <MdHome />
+        <span>Go Home</span>
+      </Button>
     </Section>
   );
 };
