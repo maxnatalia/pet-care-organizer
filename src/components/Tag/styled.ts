db3c93bd6726e0f0444bafa73455379fe17db473
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-13 10:23:23 +0200

    improved styles for Tag component - added new props $special

--- src/components/Tag/styled.ts
+++ src/components/Tag/styled.ts
@@ -1,12 +1,35 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledTag = styled(Link)`
+export const StyledTag = styled(Link)<{ $special?: boolean }>`
   margin: 10px;
   padding: 4px 10px;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.textPrimary};
   display: inline-flex;
   gap: 10px;
   flex-wrap: wrap;
   border-radius: 6px;
-  border: 4px solid yellow;
+  border: 2px solid ${({ theme }) => theme.color.textPrimary};
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.backgroundSecondary};
+    background-color: ${({ theme }) => theme.color.textPrimary};
+  }
+
+  ${({ $special }) =>
+    $special &&
+    css`
+      color: ${({ theme }) => theme.color.special};
+      border: 2px solid ${({ theme }) => theme.color.special};
+
+      &:hover {
+        color: ${({ theme }) => theme.color.backgroundPrimary};
+        background-color: ${({ theme }) => theme.color.special};
+      }
+    `}
 `;
