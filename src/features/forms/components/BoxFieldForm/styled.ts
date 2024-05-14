f52f83b00b16ffb24631d3c28e4397bebd459940
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-14 08:57:47 +0200

    updated - added new components Label and ErrorMsg styling

--- src/features/forms/components/BoxFieldForm/styled.ts
+++ src/features/forms/components/BoxFieldForm/styled.ts
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
-export const StyledBoxFieldForm = styled.div`
-  border: 2px solid yellow;
-`;
-
 export const Wrapper = styled.div<{ $row?: boolean }>`
   display: ${({ $row }) => ($row ? "flex" : "")};
   align-items: center;
   justify-content: space-around;
   flex-wrap: wrap;
+`;
+
+export const Label = styled.label`
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.textPrimary};
+`;
+
+export const ErrorMsg = styled.p`
+  margin: 0;
+  color: red;
 `;
