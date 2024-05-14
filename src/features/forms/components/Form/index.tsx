d5d8cb9422d30d1f65fa715a665eaaae33bfa818
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-14 08:55:44 +0200

    improved Buttons and added  new component Asterisk

--- src/features/forms/components/Form/index.tsx
+++ src/features/forms/components/Form/index.tsx
@@ -1,21 +1,50 @@
 import { ComponentProps, ReactNode } from "react";
 import Button from "../../../../components/Button";
-import { FieldsWrapper, StyledForm, Wrapper } from "./styled";
+import { FieldsWrapper, StyledForm, Wrapper, Asterisk } from "./styled";
+import { useNavigate } from "react-router-dom";
 
 type FormProps = {
   children: ReactNode;
+  onCancelForm: () => void;
+  editableForm: boolean;
 } & ComponentProps<typeof StyledForm>;
 
-const Form = ({ children, ...props }: FormProps) => {
+const Form = ({
+  children,
+  onCancelForm,
+  editableForm,
+  ...props
+}: FormProps) => {
+  const navigate = useNavigate();
+
   return (
     <StyledForm {...props}>
       <FieldsWrapper>{children}</FieldsWrapper>
       <Wrapper>
-        <Button type="submit">Add</Button>
-        <Button type="button" redirectPath="/">
+        <Button
+          $variety="default"
+          type="submit"
+          title={editableForm ? "Add your new pet" : "Save changes"}
+        >
+          {editableForm ? "ADD" : "SAVE"}
+        </Button>
+        <Button
+          $variety="special"
+          type="button"
+          onClick={onCancelForm}
+          title={"Exit the Form"}
+        >
           Cancel
         </Button>
+        <Button
+          $variety="default"
+          onClick={() => navigate("/")}
+          title="Go back to the HOME"
+        >
+          Go Home
+        </Button>
       </Wrapper>
+      <Asterisk>* Fields marked with an asterisk are required</Asterisk>
     </StyledForm>
   );
 };
