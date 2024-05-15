ee0fb0594dcad02e400562f041a6a78ad5815c1e
maxnatalia <Nataliamazur1988@gmail.com>
2024-05-15 07:40:48 +0200

    updated - implemented custom hook for local storage

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 import { Event, Pet } from "../types/types";
 import { initialEventsList } from "../data/initialEventsList";
 import { initialPetsList } from "../data/initialPetsList";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type AppContextType = {
   eventsList: Event[];
@@ -23,18 +24,21 @@ type AppProviderProps = {
 export const AppContext = createContext<AppContextType | null>(null);
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  const [eventsList, setEventsList] = useState<Event[]>(initialEventsList);
-  const [editableEventId, setEditableEventId] = useState("");
-  const [petsList, setPetsList] = useState<Pet[]>(initialPetsList);
+  const [eventsList, setEventsList] = useLocalStorage(
+    "events",
+    initialEventsList
+  );
+  const [petsList, setPetsList] = useLocalStorage("pets", initialPetsList);
   const [editablePetId, setEditablePetId] = useState("");
+  const [editableEventId, setEditableEventId] = useState("");
 
   const handleEditPet = (id: Pet["petId"]) => {
-    petsList.find(editablePet => editablePet.petId === id);
+    petsList.find((editablePet: Pet) => editablePet.petId === id);
     setEditablePetId(id);
   };
 
   const handleEditEvent = (id: Event["eventId"]) => {
-    eventsList.find(editableEvent => editableEvent.eventId === id);
+    eventsList.find((editableEvent: Event) => editableEvent.eventId === id);
     setEditableEventId(id);
   };
 
